6b32c702c7819cc4c3830d145f19816fbb20d493
nkwuda theophilus <theophilusonyebuchinkwuda@gmail.com>
2022-11-04 16:28:01 +0100

    api resources routing

--- src/pos/routes/api.php
+++ src/pos/routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::apiResource('/customers',);
+Route::apiResource('/purchases', CustomersController::class);
+Route::apiResource('/transactions', CustomersController::class);
+Route::apiResource('/sales', CustomersController::class);
+Route::apiResource('/receipts', ReceiptController::class);
