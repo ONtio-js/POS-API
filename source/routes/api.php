6012115983e0948b8e0b890ef7c0e9183b5f3579
ontio <ontio@nkwudas-MacBook-Pro.local>
2022-12-31 20:26:33 +0100

    quick fix

--- source/routes/api.php
+++ source/routes/api.php
@@ -1,13 +1,5 @@
 <?php
 
-use App\Http\Controllers\V1\AdminstratorController;
-use App\Http\Controllers\V1\CustomerController;
-use App\Http\Controllers\V1\PaymentController;
-use App\Http\Controllers\V1\ProductDetailController;
-use App\Http\Controllers\V1\PurchaseController;
-use App\Http\Controllers\V1\ReceiptController;
-use App\Http\Controllers\V1\SaleController;
-use App\Http\Controllers\V1\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,15 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
-Route::prefix('v1')
-->group(function(){
-    Route::apiResource('/customer',CustomerController::class);
-    Route::apiResource('/adminstrator',AdminstratorController::class);
-    Route::apiResource('/purchase',PurchaseController::class);
-    Route::apiResource('/sales',SaleController::class);
-    Route::apiResource('/receipts',ReceiptController::class);
-    Route::apiResource('/payment',PaymentController::class);
-    Route::apiResource('products',ProductDetailController::class);
-    Route::apiResource('/transaction',TransactionController::class);
 });
